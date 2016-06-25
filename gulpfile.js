77a96ce93399cd3555064ea8dfaae59577fb8482
Salva Crespo <cresponline@gmail.com>
2016-06-26 01:06:56 +0200

    adding gulp-clean-css module

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,15 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     maps = require('gulp-sourcemaps'),
     insert = require('gulp-insert'),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function(){
     return gulp.src('scss/build.scss')
         .pipe(maps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(insert.wrap('<style>', '</style></head>'))
         .pipe(maps.write('./'))
         .pipe(gulp.dest('css'));
